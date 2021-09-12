cd8c7d6462c33d34f09bee09ccfcf33730a697ce
bmertali <mertalibarin@gmail.com>
2021-09-13 00:56:57 +0300

    Spring Full XML Configuration - IoC and DI

---
+++ spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "You need to work hard!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
