8cdaaa937fb1bb8d88fde7b9859ab201012b591e
bmertali <mertalibarin@gmail.com>
2021-09-15 04:18:15 +0300

    Spring Annotations

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,65 @@
+package com.luv2code.springdemo;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+	
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanStuff()");
+	}
+	
+	
+	// define a setter method
+//	@Autowired
+//	public void setFortuneService(FortuneService fortuneService) {
+//		System.out.println(">> TennisCoach: inside setFortuneService() method");
+//		this.fortuneService = fortuneService;
+//	}
+	
+//	@Autowired
+//	public void doSomeCrazyStuff(FortuneService fortuneService) {
+//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//		this.fortuneService = fortuneService;
+//	}
+	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	@Override
+	public String getDailyWorkOut() {
+		return "Practice your backhand volley";
+	}
+
+	
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
